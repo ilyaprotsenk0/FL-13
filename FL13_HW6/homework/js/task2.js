e96d84db40728fd477ba0274c4c3be5b12b3d3be
Ilya Protsenko <iliafreshfish@gmail.com>
2020-04-23 04:07:13 +0300

    Task 2 have done

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,18 @@
-// Your code goes here
+let inputWord = prompt('Enter any word'),
+    middleChar = '';
+
+const wordMidpoint = (inputWord.length - 1) / 2;
+    
+if (inputWord.trim('') !== '') {
+
+    if (inputWord.length % 2 !== 0) {
+        middleChar = inputWord[wordMidpoint];
+    } else {
+        middleChar += inputWord[Math.round(wordMidpoint - 1)];
+        middleChar += inputWord[Math.round(wordMidpoint)];
+    }
+    alert(middleChar);
+
+} else {
+    alert('Invalid value');
+}

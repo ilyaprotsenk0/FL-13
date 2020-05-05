7ae037c4831d8e819bc7147db15ad63af717d183
Ilya Protsenko <iliafreshfish@gmail.com>
2020-05-05 20:24:38 +0300

    Tasks [6-11] were done. Minor formating fixes were added

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -1,9 +1,9 @@
 const convert = (...args) => {
-    let argsArr = [...args];
-    for (let i = 0; i < argsArr.length; i++) {
-        typeof argsArr[i] === 'string' ? argsArr[i] = parseInt(argsArr[i]) : argsArr[i] = '' + argsArr[i];
+    let converted = [];
+    for (let i = 0; i < [...args].length; i++) {
+        typeof [...args][i] === 'string' ? converted.push(parseInt([...args][i])) : converted.push('' + [...args][i]);
     }
-    return argsArr;
+    return converted;
 }
 
 console.log(convert('1', 2, '3'));
@@ -34,15 +34,15 @@ console.log(filterArray([2, 5, 8], el => el % 2 === 0));
 
 const containValue = (arr, value) => {
     let contains = false;
-    executeforEach(arr, function (el) {
+    executeforEach(arr, el => {
         el === value ? contains = true : '';
     });
     return contains;
 };
 
-console.log(containValue([12, 4, 5], 6));
+console.log(containValue([12, 4, 5], 5));
 
-function flipOver(str) {
+const flipOver = str => {
     let reverseStr = '';
     for (let i = 1; i < str.length + 1; i++) {
         reverseStr += str[str.length - i];
@@ -51,3 +51,52 @@ function flipOver(str) {
 }
 
 console.log(flipOver('собака'));
+
+const makeListFromRange = arr => {
+    let list = [];
+    for (let i = arr[0]; i < arr[1] + 1; i++) {
+        list.push(i);
+    }
+    return list;
+}
+
+console.log(makeListFromRange([1, 5]));
+
+const fruits = [
+    { name: 'apple', weight: 0.5 },
+    { name: 'pineapple', weight: 2 }
+];
+
+const getArrayOfKeys = (arr, key) => {
+    let keyArr = [];
+    executeforEach(arr, el => {
+        keyArr.push(el[key]);
+    });
+    return keyArr;
+}
+
+console.log(getArrayOfKeys(fruits, 'name'));
+
+const UP_LIM = 20, 
+      LOW_LIM = 10;
+const substitute = arr => mapArray(arr, el => el < UP_LIM && el > LOW_LIM ? '*' : el);
+
+console.log(substitute([58, 14, 48, 12, 31, 19, 10]));
+
+const date = new Date(2020, 0, 2);
+const getPastDay = (date, past) => date.getDate(date.setDate(date.getDate() - past));
+
+console.log(getPastDay(date, 365));
+
+const formatDate = date => {
+    let year = date.getFullYear(),
+        month = date.getMonth(),
+        day = date.getDate(),
+        hour = date.getHours(),
+        minute = date.getMinutes();
+
+    const zero = p => p < 10 ? '0' + p : p;
+    return `${year}/${zero(month)}/${zero(day)} ${zero(hour)}:${zero(minute)}`;
+}
+
+console.log(formatDate(new Date()));

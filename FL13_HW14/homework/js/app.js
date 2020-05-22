d924846601a39003534dcb7a7b2ce80ee53d5245
Ilya Protsenko <iliafreshfish@gmail.com>
2020-05-22 13:28:17 +0300

    Code was refactored (v.2)

--- FL13_HW14/homework/js/app.js
+++ FL13_HW14/homework/js/app.js
@@ -1,17 +1,19 @@
 let Student = function(name, email) {
-    let studentName = name,
-        studentEmail = email;
-    let homeworkResults = [];
+    this.name = name;
+    this.email = email;
+    this.homeworkResults = [];
     return {
-        getName: () => studentName,
-        getEmail: () => studentEmail,
-        addHomeworkResult: (topic, success) => homeworkResults.push({topic, success}),
-        getHomeworkResults: () => homeworkResults
+        getName: () => this.name,
+        getEmail: () => this.email,
+        addHomeworkResult: (topic, success) => {
+            this.homeworkResults.push({topic, success})
+        },
+        getHomeworkResults: () => this.homeworkResults
     };
 }
 
 let FrontEndLab = function (studentsArr, failedHomeworksLimit) {
-    let studentsList = studentsArr.map(stud => new Student(stud.name, stud.email));
+    let studentsList = studentsArr.map(student => new Student(student.name, student.email));
     return {
         printStudentsList: () => {
             for (let student of studentsList) {
